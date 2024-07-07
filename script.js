f91be6993a075acce28f60aeb73d4b38eb96b131
Muhammad Tayyab <131167152+786Tayyab@users.noreply.github.com>
2024-07-07 20:27:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function display(value) 
+{
+    document.getElementById('result').value += value;
+}
+
+      
+function clearDisplay() {
+    document.getElementById('result').value = '';
+}
+
+        
+function calculate() 
+{
+    try {
+        let result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } 
+    catch (error) 
+    {
+        document.getElementById('result').value = 'Error';
+    }
+}
